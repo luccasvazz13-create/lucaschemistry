0209cfb6ee183f7d2e68caa3d57eb11f13d1ce6e
luccasvazz13-create <luccasvazz13@gmail.com>
2026-08-17 21:23:20 -0300

    Update vite.pages.config.ts

--- vite.pages.config.ts
+++ vite.pages.config.ts
@@ -1,11 +1,10 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/lucaschemistry/",
   plugins: [react()],
+  base: "/",
   build: {
     outDir: "pages-dist",
-    emptyOutDir: true,
   },
 });
